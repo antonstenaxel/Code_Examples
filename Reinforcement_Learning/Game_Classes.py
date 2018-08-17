b699f9eb84033795e80af4766094bdc7fcc3e566
antonstenaxel <antkarls@student.chalmers.se>
2018-08-17 12:54:27 +0200

    Adding ongoing project trying to use reinofrcment learning to train a snake in the classic game snake using deep q-network. So far game has been written.

---
+++ Reinforcement_Learning/Game_Classes.py
@@ -0,0 +1,96 @@
+
+from pygame.locals import *
+import pygame, sys, os, time, random
+import numpy as np
+
+
+# CONSTANTS
+SCALE = 20
+SCREEN_SIZE = 300
+N_TILES = SCREEN_SIZE/SCALE-1
+START_POS = np.array([N_TILES // 2 ,N_TILES // 2 ],dtype=np.int)
+BACKGROUND_COLOR = (51, 51, 51)
+SNAKE_HEAD_COLOR = (255, 50, 50)
+SNAKE_BODY_COLOR = (255, 100, 100)
+APPLE_COLOR = (100, 255, 100)
+ACTIONS = np.array([[0,-1],[0,1],[-1,0],[1,0]],dtype=np.int)
+
+
+class Snake:
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+
+        self.direction = random.choice(ACTIONS)
+        self.head_position = np.copy(START_POS)
+        self.body_position = np.vstack([self.head_position - i*self.direction for i in range(1,5)])
+        self.grow = False
+
+    def eat_apple(self):
+        self.grow = True
+
+    def eat_self(self):
+        self.reset()
+
+    def crash(self):
+        self.reset()
+
+    def turn(self, direction):
+        if not np.all(direction == -self.direction):
+            self.direction = direction
+
+    def grow(self):
+        self.body_position = np.append(self.body_position)
+
+    def update(self):
+
+
+        if self.grow:
+            self.body_position = np.append([self.head_position], self.body_position, axis = 0)
+            self.grow = False
+        else:
+            self.body_position[1:,:] = self.body_position[:-1,:]
+            self.body_position[0,:] = self.head_position
+
+        self.head_position += self.direction
+
+
+class Apple:
+
+    def __init__(self):
+        self.relocate()
+
+
+    def relocate(self):
+        self.position = np.random.randint(0,N_TILES,size=[2])
+
+class Graphics:
+
+    def __init__(self):
+
+
+        pygame.init()
+        self.s = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+        pygame.display.set_caption('Snake')
+
+        self.apple_image = pygame.Surface((SCALE, SCALE))
+        self.apple_image.fill(APPLE_COLOR)
+
+        self.snake_head_image = pygame.Surface((SCALE, SCALE))
+        self.snake_head_image.fill(SNAKE_HEAD_COLOR)
+
+        self.snake_body_image = pygame.Surface((SCALE, SCALE))
+        self.snake_body_image.fill(SNAKE_BODY_COLOR)
+
+
+    def render(self,snake_head,snake_body,apple_pos):
+        self.s.fill(BACKGROUND_COLOR)
+        self.s.blit(self.apple_image,(apple_pos[0]*SCALE,apple_pos[1]*SCALE))
+
+        self.s.blit(self.snake_head_image,(snake_head[0]*SCALE,snake_head[1]*SCALE))
+        for bodypart in snake_body:
+            self.s.blit(self.snake_body_image,(bodypart[0]*SCALE,bodypart[1]*SCALE))
+
+        pygame.display.update()
---
+++ Reinforcement_Learning/main.py
@@ -0,0 +1,46 @@
+from pygame.locals import *
+import pygame, time
+from Game_Classes import Apple, Snake, Graphics, SCREEN_SIZE,SCALE, ACTIONS, N_TILES
+import numpy as np
+
+
+if __name__ == "__main__":
+
+
+    apple = Apple()
+    snake = Snake()
+    graphics = Graphics()
+
+    while True:  # Main game loop
+
+        time.sleep(0.1)
+
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                sys.exit(0)
+            elif e.type == KEYDOWN:
+                if e.key == K_UP:
+                    snake.turn(ACTIONS[0])
+                elif e.key == K_DOWN:
+                    snake.turn(ACTIONS[1])
+                elif e.key == K_LEFT:
+                    snake.turn(ACTIONS[2])
+                elif e.key == K_RIGHT:
+                    snake.turn(ACTIONS[3])
+
+        snake.update()
+        graphics.render(snake.head_position, snake.body_position, apple.position)
+
+        # Check wall collision
+        if np.any(snake.head_position < 0) or np.any(snake.head_position > N_TILES):
+            snake.crash()
+
+        # Check if snake eats itself
+        for bodypart in snake.body_position:
+            if np.all(snake.head_position == bodypart):
+                snake.eat_self()
+
+        # Check if snake eats apple
+        if np.all(apple.position == snake.head_position):
+            snake.eat_apple()
+            apple.relocate()
